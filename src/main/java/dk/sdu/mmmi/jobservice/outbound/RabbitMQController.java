cec3879f0515bf0bb080b14187141da4459618c3
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-02-01 20:41:02 +0100

    serializing object

--- src/main/java/dk/sdu/mmmi/jobservice/outbound/RabbitMQController.java
+++ src/main/java/dk/sdu/mmmi/jobservice/outbound/RabbitMQController.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.jobservice.outbound;
 
+import com.google.gson.Gson;
 import dk.sdu.mmmi.jobservice.outbound.config.RabbitMQConfig;
 import dk.sdu.mmmi.jobservice.service.interfaces.RabbitMqService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Service;
 public class RabbitMQController implements RabbitMqService {
     private final RabbitTemplate rabbitTemplate;
 
+    private Gson gson = new Gson();
+
     public void sendMessage(Object object) {
-        rabbitTemplate.convertAndSend(RabbitMQConfig.NEW_JOB_EXCHANGE, RabbitMQConfig.NEW_JOB_ROUTING_KEY, object);
+        rabbitTemplate.convertAndSend(RabbitMQConfig.NEW_JOB_EXCHANGE, RabbitMQConfig.NEW_JOB_ROUTING_KEY, gson.toJson(object));
     }
 }
